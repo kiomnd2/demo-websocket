b13aaaa5bc3980d4047a89385b259bf13d805b9a
kiomnd2 <kiomnd2@naver.com>
2020-10-20 15:11:11 +0900

    웹소캣 연결 및 재연결

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="page-container">
+      <md-app md-mode="reveal">
+        <md-app-toolbar :class="state">
+          <span class="md-title">Websocket Test</span>
+        </md-app-toolbar>
+        <md-app-content>
+          <div v-if="connected">
+            <md-progress-bar md-mode="indeterminate"></md-progress-bar>
+            <md-progress-bar class="md-accent" md-mode="indeterminate"></md-progress-bar>
+          </div>
+        </md-app-content>
+      </md-app>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      state: 'md-default',
+      connected: false
+    }
+  },
+  methods: {
+    init () {
+      const ws = new WebSocket('ws://localhost:9999/websocket')
+      this.onOpen(ws)
+      this.onClose(ws)
+    },
+    onOpen (ws) {
+      ws.onopen = (message) => {
+        clearTimeout(this.reconnecting())
+        console.log('connected!!')
+        this.state = 'md-primary'
+        console.log(message)
+        this.connected = false
+      }
+    },
+    onClose (ws) {
+      ws.onclose = (message) => {
+        console.log('closed')
+        this.reconnecting()
+        this.state = 'md-default'
+        console.log(message)
+        this.connected = true
+      }
+    },
+    reconnecting () {
+      return setTimeout(() => {
+        console.log('연결을 재시도 합니다')
+        this.init()
+      }, 5000)
+    }cd
+    ls
+  },
+  created () {
+    this.init()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
